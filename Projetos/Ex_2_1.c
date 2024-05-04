2290b5e57d950e56e9f9cffb4ae385168a6c6c3f
Gerson Santos <gersones@outlook.com>
2024-05-03 21:27:14 -0300

    a

--- Projetos/Ex_2_1.c
+++ Projetos/Ex_2_1.c
@@ -7,7 +7,7 @@ int main(){
 	printf("Valor real: %f.\n", 3.14159265);
 	printf("Valor real: float. %f.\n", 3.14159265);
 	printf("Valor real com apenas duas casas: %.2f.\n", 3.14159265);
-	printf("Dado de texto: %c.\n", 'a');
+	printf("Dado de Caracter: %c.\n", 'a');
 	printf("Dado de texto: %s.\n", "testando");
 
 }
